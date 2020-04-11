8b862b661c9b048fcb585e2e65b75ae63eb880bd
Kohaku Cao <kohaku@hepnovel.com>
2020-04-11 13:31:19 +0800

    First Upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta charset="utf8" />
+		<title>Nebula MUN System</title>
+		<link rel="stylesheet" href="/css/bootstrap.min.css">
+	</head>
+	
+	<body>
+		<h1>niupi</h1>
+		<script src="js/jquery-3.5.0.min.js" type="application/javascript"></script>
+		<script src="js/bootstrap.bundle.min.js" type="application/javascript"></script>
+	</body>
+</html>
